5a6d4040d57b4e940c39ab2b35c09930f62ba9a2
radheyyyyyy <mahiradhey0204@gmail.com>
2026-03-25 13:43:16 +0530

    activate.tsx added

---
+++ app/routes/app.activate.tsx
@@ -0,0 +1,41 @@
+
+import { authenticate } from "../shopify.server";
+import { useActionData, useSubmit } from "react-router";
+
+// ✅ action handles POST — correct for mutations
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+
+  const res = await admin.graphql(`
+    #graphql
+    mutation {
+      cartTransformCreate(functionHandle: "cart-attribute-function") {
+        cartTransform {
+          id
+          functionId
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `);
+
+  const { data } = await res.json();
+  console.log(data)
+  return {data};
+};
+
+export default function Activate() {
+  const actionData = useActionData();
+    const submit=useSubmit();
+  return (
+    <s-page>
+     
+        <s-button variant="primary" onClick={()=>{submit({method:'post'})}}>Activate Cart Transform</s-button>
+      {/* Shows response after submit */}
+      {actionData && <pre>{JSON.stringify(actionData, null, 2)}</pre>}
+    </s-page>
+  );
+}
